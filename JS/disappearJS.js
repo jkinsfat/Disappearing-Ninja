10e367a398fd16282c3c81f5d36279a6ef5a58e2
jkinsfat <jkinsfat@w.edu>
2017-05-30 13:03:50 -0700

    Img disappear when clicked. Restore button makes them visible again. Ninja counter displays and updates correctly

--- JS/disappearJS.js
+++ JS/disappearJS.js
@@ -1,3 +1,20 @@
 $('Document').ready(function() {
-    
+    var invisibleImg = 0;
+    $('img').click(function() {
+        invisibleImg++;
+        $(this).css("visibility", "hidden");
+        if (invisibleImg < 8) {
+            $('h4').text(invisibleImg + " ninjas have vanished into the mist...");
+        } else {
+            $('h3').css("visibility", "hidden");
+            $('h4').text("You are alone amidst the swirls of taunting mist");
+        }
+    })
+
+    $('.restore').click(function() {
+        $('img').css("visibility", "visible");
+        $('h3').css("visibility", "visible");
+        invisibleImg = 0;
+        $('h4').text('');
+    })
 })
